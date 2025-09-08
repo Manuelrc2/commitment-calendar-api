d611c5e6838caa401dc74c084045a4c5a0fcfb89
Manueldlrc2 <154353144+Manuelrc2@users.noreply.github.com>
2025-09-08 13:49:28 +0200

    edit appointments

--- Interfaces/IAppointmentService.cs
+++ Interfaces/IAppointmentService.cs
@@ -7,5 +7,6 @@ namespace commitment_calendar_api.Interfaces
         Task<MonthCalendar> GetCalendarByUserAndDate(string userId, DateTime date, string timezone);
         Task CreateAppointment(string userId, AppointmentDto appointmentDto);
         Task DeleteAppointment(string userId, long id);
+        Task UpdateAppointment(string userId, AppointmentDto appointmentDto);
     }
 }
